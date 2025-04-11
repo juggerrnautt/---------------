749d0205b81f41b049a4a43eebdda0b2a6561b44
juggerrnautt <sasasmrnov6@gmail.com>
2025-04-11 19:57:04 +0200

    імпортував бібліотеку PyQt5

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("hello world")
+from PyQt5.QtCore import Qt                                                                       
+from PyQt5.QtWidgets import (                                                                     
+    QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QRadioButton, QInputDialog, QTextEdit, QLineEdit, QListWidget
+)    
+
+app = QApplication([])
+main_window = QWidget()
+main_window.resize(700, 700)
+main_window.setWindowTitle("розумні замітки")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main_window.show()
+app.exec_()
